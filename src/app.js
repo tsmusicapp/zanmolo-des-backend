09a7295e519acb0489da7e79fa523fc37b7fe792
mishal ahammed <crazymichuzz786@gmail.com>
2025-12-21 20:46:58 +0530

    updated version v1

--- src/app.js
+++ src/app.js
@@ -80,7 +80,7 @@ app.use('/v1', routes);
 
 
 app.get('/', (req, res) => {
-  res.send('Welcome! Backend Music App Running Normaly.');
+  res.send('Welcome! Backend Music App Running Normaly. v1');
 });
 
 // Initialize attachment cleanup scheduler (runs every 24 hours)
